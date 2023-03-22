83652c56a8c979bcbb2538d17068231756f3e12f
Jesus <jesusmoralescanete@gmail.com>
2023-03-22 11:52:55 +0100

    arreglado fallo en la redireccion

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 import FormPage from "./components/FormPage.vue"
-import { createRouter } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router"
 
 const routes = [
-  {path: '*', redirect: '/home'},
-  {path: '/home',  name: 'form-page', component: FormPage},
+  {path: '/:catchAll(.*)', redirect: '/home'},
+  {path: '/home',  name: 'form-page',component: FormPage},
 ]
 
+const history = createWebHistory()
+
 export const router = createRouter({
+  history,
   routes,
 })
